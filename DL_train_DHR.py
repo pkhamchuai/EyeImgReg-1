cb748ad5a4fd743985e1beb7780e00d0bf84009c
pkhamchuai <pakpoom_kh@cmu.ac.th>
2023-10-30 14:30:10 +0700

    added DHR

--- DL_train_DHR.py
+++ DL_train_DHR.py
@@ -13,12 +13,14 @@ from datetime import datetime
 from tqdm import tqdm
 
 import torch
+from torchvision import transforms
 from torch import nn, optim
+import torch.nn.functional as F
+from torch.utils import data
 
 from utils.utils0 import *
 from utils.utils1 import *
 from utils.utils1 import ModelParams, DL_affine_plot
-from utils.SPaffineNet3 import SP_AffineNet3
 from utils.datagen import datagen
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -29,6 +31,74 @@ if int(cv2.__version__[0]) < 3: # pragma: no cover
   print('Warning: OpenCV 3 is not installed')
 
 image_size = 256
+
+from networks import affine_network_simple as an
+from utils.SuperPoint import SuperPointFrontend
+from utils.utils1 import transform_points_DVF
+
+class SP_DHR_Net(nn.Module):
+    def __init__(self, model_params):
+        super(SP_DHR_Net, self).__init__()
+        self.superpoint = SuperPointFrontend('utils/superpoint_v1.pth', nms_dist=4,
+                          conf_thresh=0.015, nn_thresh=0.7, cuda=True)
+        self.affineNet = an.load_network(device)
+        self.nn_thresh = 0.7
+        self.model_params = model_params
+        print("\nRunning new version (not run SP on source image)")
+
+    def forward(self, source_image, target_image):
+        # source_image = source_image.to(device)
+        # target_image = target_image.to(device)
+
+        # print('source_image: ', source_image.shape)
+        # print('target_image: ', target_image.shape)
+        points1, desc1, heatmap1 = self.superpoint(source_image[0, 0, :, :].cpu().numpy())
+        points2, desc2, heatmap2 = self.superpoint(target_image[0, 0, :, :].cpu().numpy())
+
+        if self.model_params.heatmaps == 0:
+            affine_params = self.affineNet(source_image, target_image)
+        elif self.model_params.heatmaps == 1:
+            print("This part is not yet implemented.")
+            # affine_params = self.affineNet(source_image, target_image, heatmap1, heatmap2)
+
+        # transform the source image using the affine parameters
+        # using F.affine_grid and F.grid_sample
+        transformed_source_affine = tensor_affine_transform(source_image, affine_params)
+        points1_2, desc1_2, heatmap1_2 = self.superpoint(transformed_source_affine[0, 0, :, :].detach().cpu().numpy())
+
+        # match the points between the two images
+        tracker = PointTracker(5, nn_thresh=0.7)
+        try:
+            matches = tracker.nn_match_two_way(desc1, desc2, nn_thresh=self.nn_thresh)
+        except:
+            # print('No matches found')
+            # TODO: find a better way to do this
+            try:
+                while matches.shape[1] < 3 and self.nn_thresh > 0.1:
+                    self.nn_thresh = self.nn_thresh - 0.1
+                    matches = tracker.nn_match_two_way(desc1, desc2, nn_thresh=self.nn_thresh)
+            except:
+                return transformed_source_affine, affine_params, [], [], [], [], [], [], []
+
+        # take the elements from points1 and points2 using the matches as indices
+        matches1 = np.array(points1[:2, matches[0, :].astype(int)])
+        matches2 = np.array(points2[:2, matches[1, :].astype(int)])
+        # matches1_2 = np.array(points1_2[:2, matches[0, :].astype(int)])
+        # print('matches1', matches1)
+        # print('matches2', matches2)
+        # print('matches1_2', matches1_2)
+
+        # try:
+        #     matches1_2 = points1_2[:2, matches[0, :].astype(int)]
+        # except:
+        # print(affine_params.cpu().detach().shape, transformed_source_affine.shape)
+        matches1_2 = transform_points_DVF(torch.tensor(matches1), 
+                        affine_params.cpu().detach(), transformed_source_affine)
+
+        # transform the points using the affine parameters
+        # matches1_transformed = transform_points(matches1.T[None, :, :], affine_params.cpu().detach())
+        return transformed_source_affine, affine_params, matches1, matches2, matches1_2, \
+            desc1_2, desc2, heatmap1_2, heatmap2
 
 # Define training function
 def train(model, model_params, timestamp):
@@ -331,7 +401,7 @@ if __name__ == '__main__':
     print('Train set: ', [x.shape for x in next(iter(train_dataset))])
     print('Test set: ', [x.shape for x in next(iter(test_dataset))])
 
-    model = SP_AffineNet3(model_params).to(device)
+    model = SP_DHR_Net(model_params).to(device)
     print(model)
 
     parameters = model.parameters()
@@ -363,7 +433,7 @@ if __name__ == '__main__':
         print(loss_list[i])
 
     model_save_path = "trained_models/"
-    model_name_to_save = model_save_path + f"model3_{model_params.get_model_code()}_{timestamp}.pth"
+    model_name_to_save = model_save_path + f"DHR_{model_params.get_model_code()}_{timestamp}.pth"
     print(model_name_to_save)
     torch.save(model.state_dict(), model_name_to_save)
 
